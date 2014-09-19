050f6917a4bfa0a3cd2fe9564373dc3b681469d0
ChivialK <chivialk9913@gmail.com>
2014-09-19 16:12:35 +0800

    Initial Commit

---
+++ Note/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Note
+//
+//  Created by RavenC on 2014/9/19.
+//  Copyright (c) 2014年 ChivialK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Note/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Note
+//
+//  Created by RavenC on 2014/9/19.
+//  Copyright (c) 2014年 ChivialK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
